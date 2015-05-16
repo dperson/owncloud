22cec389a90d603be24e5ce5d9e22bf84771232d
David Personette <dperson@gmail.com>
2015-05-16 14:22:44 -0400

    owncloud.sh fix behavior on config change when daemon is running

--- owncloud.sh
+++ owncloud.sh
@@ -69,7 +69,9 @@ chown root:www-data /var/www/owncloud/data/.htaccess 2>/dev/null
 chmod 0644 /var/www/owncloud/.htaccess /var/www/owncloud/data/.htaccess \
             2>/dev/null
 
-if [[ $# -ge 1 && -x $(which $1 2>&-) ]]; then
+if ps -ef | grep -q lighttpd; then
+    echo "Service already running, please restart container to apply changes"
+elif [[ $# -ge 1 && -x $(which $1 2>&-) ]]; then
     exec "$@"
 elif [[ $# -ge 1 ]]; then
     echo "ERROR: command not found: $1"
